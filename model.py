bcc7be8a61807ddeefcfe885adb680ea50eefe2b
LucasBoTang <botang@mie.utoronto.ca>
2022-08-27 21:33:48 -0400

    Update model

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ A 4-layer fully-connected ReLU-activated network
 
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 class fcNet(nn.Module):
 
@@ -19,21 +20,20 @@ class fcNet(nn.Module):
         """
         super(fcNet, self).__init__()
         # main layers
-        self.main = nn.Sequential(
-            nn.Linear(dim_features, dim_labels),
-            nn.ReLU(),
-            nn.Linear(dim_labels, dim_labels),
-            nn.ReLU(),
-            nn.Linear(dim_labels, dim_labels),
-            nn.ReLU()
-        )
+        self.fc1 = nn.Linear(dim_features, dim_labels)
+        self.fc2 = nn.Linear(dim_labels, dim_labels)
+        self.fc3 = nn.Linear(dim_labels, dim_labels)
+        self.fc4 = nn.Linear(dim_labels, dim_labels)
         # heads for multitask
         self.heads = [nn.Linear(dim_labels, dim_labels) for _ in range(n_tasks)]
         self.mse = nn.MSELoss()
 
     def forward(self, x, ys):
         # prediction
-        h = self.main(x)
+        h = F.relu(self.fc1(x))
+        h = F.relu(self.fc2(h))
+        h = F.relu(self.fc3(h))
+        h = F.relu(self.fc4(h))
         # mse loss per task
         loss = []
         for i, head in enumerate(self.heads):
