9e999efecc4e5345dcb077895db07e30bc30a1ad
LucasBoTang <botang@mie.utoronto.ca>
2022-08-29 01:38:16 -0400

    Update model

--- model.py
+++ model.py
@@ -26,17 +26,39 @@ class fcNet(nn.Module):
         self.fc4 = nn.Linear(dim_labels, dim_labels)
         # heads for multitask
         self.heads = nn.ModuleList([nn.Linear(dim_labels, dim_labels) for _ in range(n_tasks)])
-        self.mse = nn.MSELoss()
 
-    def forward(self, x, ys):
+    def forward(self, x):
         # prediction
         h = F.relu(self.fc1(x))
         h = F.relu(self.fc2(h))
         h = F.relu(self.fc3(h))
         h = F.relu(self.fc4(h))
         # mse loss per task
-        loss = []
+        ys = []
         for i, head in enumerate(self.heads):
-            yp = head(h)
-            loss.append(self.mse(ys[:,i], yp))
+            y = head(h)
+            ys.append(y)
+        ys = torch.stack(ys, axis=1)
+        return ys
+
+
+class mtlNet(nn.Module):
+
+    def __init__(self, net):
+        """
+        Args:
+             net (nn.Module): network with multiple heads
+             n_tasks (int): number of tasks
+        """
+        super(mtlNet, self).__init__()
+        self.net = net
+        self.mse = nn.MSELoss()
+
+    def forward(self, x, y):
+        # prediction
+        yp = self.net(x)
+        # loss
+        loss = []
+        for i in range(yp.shape[1]):
+            loss.append(self.mse(y[:,i], yp[:,i]))
         return torch.stack(loss)
